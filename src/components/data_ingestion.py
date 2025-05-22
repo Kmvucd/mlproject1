f4440289d2d73fcb8cdacef075b64362b39ac8ed
kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2025-05-22 21:09:11 +0100

    Added Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,12 +2,13 @@ import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
-
-
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransormation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -46,4 +47,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransormation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
