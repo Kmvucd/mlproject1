8e8b1162b779f62a955d71ecc3f30d97e09683c0
kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2025-05-23 11:32:52 +0100

    Added Model Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,13 +12,14 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
     
-class DataTransormation:
+class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
         
